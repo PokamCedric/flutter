1fff88fae86fde65ffeb52f56b1cd6f2a7878b55
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-07-13 02:01:26 +0200

    Refactore responsiveness

--- data_table_template/lib/jobs_page.dart
+++ data_table_template/lib/jobs_page.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:job_listings/bloc/data_table/data_table_bloc.dart';
 import 'package:job_listings/bloc/job/job_bloc.dart';
+import 'package:job_listings/core/presentation/extensions/responsive_extension.dart';
 import 'package:job_listings/models/dropdown_filter_model.dart';
 import 'package:job_listings/models/filter_model.dart';
 import 'package:job_listings/utils/data_table/data_table.dart';
 import 'package:job_listings/utils/data_table/data_table_widget.dart';
 import 'package:job_listings/utils/filter/filter_widget.dart';
 import 'package:job_listings/bloc/filter/filter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class JobListingsPage extends StatelessWidget {
   const JobListingsPage({super.key});
@@ -30,23 +32,6 @@ class JobListingsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mediaWidth = MediaQuery.of(context).size.width;
-    int filtersPerLine = 1;
-    bool isDesktop;
-
-    if(mediaWidth > 1100.0) {
-      isDesktop = true;
-      filtersPerLine = 1;
-    }else if( mediaWidth > 700.0){
-      isDesktop = false;
-      filtersPerLine = 2;
-    }else{
-      isDesktop = false;
-      filtersPerLine = 1;
-    }
-
-    final double containerWidth = isDesktop ? 800.0 : mediaWidth;
-    final double filterWidth = isDesktop ? 250.0 : mediaWidth - 50.0;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -63,29 +48,37 @@ class JobListingsView extends StatelessWidget {
             builder: (context, filterState) {
               return BlocBuilder<DataTableBloc, DataTableState>(
                 builder: (context, dataTableState) {
-                  final totalPages = (state.totalHits / dataTableState.rowsPerPage).ceil();
 
                   return SingleChildScrollView(
                     padding: const EdgeInsets.only(top: 50.0),
-                    child: isDesktop
-                        ? desktopView(
-                            context,
-                            totalPages,
-                            containerWidth,
-                            filterWidth,
-                            state,
-                            filterState,
-                            dataTableState,
-                          )
-                        : phoneView(
-                            context,
-                            totalPages,
-                            filterWidth,
-                            state,
-                            filterState,
-                            dataTableState,
-                            filtersPerLine: filtersPerLine
-                          ),
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: ResponsiveRowColumn(
+                                columnMainAxisAlignment: MainAxisAlignment.center,
+                                columnCrossAxisAlignment: CrossAxisAlignment.center,
+                                rowMainAxisAlignment: MainAxisAlignment.center,
+                                rowCrossAxisAlignment: CrossAxisAlignment.center,
+                                columnSpacing: 32,
+                                rowSpacing: 32,
+                                layout: context.isDisplayLargerThanSmallDesktop
+                                    ? ResponsiveRowColumnType.ROW
+                                    : ResponsiveRowColumnType.COLUMN,
+                                children: [
+                                  ResponsiveRowColumnItem(
+                                    rowOrder: 1,
+                                    columnOrder: 2,
+                                    rowFlex: 3,
+                                    child: _dataTableLayout(context),
+                                  ),
+                                  ResponsiveRowColumnItem(
+                                    rowFlex: 1,
+                                    rowOrder: 2,
+                                    columnOrder: 1,
+                                    child: _filterLayout(context),
+                                  ),
+                                ],
+                              ),
+                    ),
                   );
                 },
               );
@@ -96,41 +89,50 @@ class JobListingsView extends StatelessWidget {
     );
   }
 
-  FilterWidget buildFilterWidget(
-    JobListingsState state,
-    double filterWidth,
+  Widget _filterLayout(
     BuildContext context,
     {int filtersPerLine = 1}
   ) {
-    return FilterWidget(
-      totalItems: state.filteredJobs.length,
-      filterWidth: filterWidth,
-      filtersPerLine: filtersPerLine,
-      filters: getDropdownFilterModels(),
-      onFilterChanged: (filters) =>
-          context.read<JobListingsBloc>().add(FilterJobsEvent(FilterModel.fromJson(filters))),
+    return BlocBuilder<JobListingsBloc, JobListingsState>(
+            builder: (context, state) {
+              return FilterWidget(
+                      totalItems: state.filteredJobs.length,
+                      filterWidth: context.filterWidth,
+                      filtersPerLine: filtersPerLine,
+                      filters: getDropdownFilterModels(),
+                      onFilterChanged: (filters) =>
+                          context.read<JobListingsBloc>().add(FilterJobsEvent(FilterModel.fromJson(filters))),
+                    );
+            }
     );
   }
 
-  DataTableWidget buildDataTableWidget(
-    JobListingsState state,
-    double containerWidth,
-    DataTableState dataTableState,
-    int totalPages,
+  Widget _dataTableLayout(
     BuildContext context,
   ) {
-    return DataTableWidget(
-      data: state.filteredJobs.map((job) => job.toJson()).toList(),
-      containerWidth: containerWidth,
-      rowsPerPage: dataTableState.rowsPerPage,
-      currentPage: dataTableState.currentPage,
-      totalPages: totalPages,
-      totalHits: state.totalHits,
-      availableRowsPerPage: const [5, 10, 25, 50],
-      onPageChanged: (newPage) => context.read<DataTableBloc>().add(ChangePageEvent(newPage)),
-      onRowsPerPageChanged: (newRowsPerPage) =>
-          context.read<DataTableBloc>().add(ChangeRowsPerPageEvent(newRowsPerPage!)),
-      columns: getTableColumns(containerWidth),
+    const double containerWidth = 800.0;
+    return BlocBuilder<JobListingsBloc, JobListingsState>(
+            builder: (context, state) {
+              return BlocBuilder<DataTableBloc, DataTableState>(
+                builder: (context, dataTableState) {
+                  final totalPages = (state.totalHits / dataTableState.rowsPerPage).ceil();
+
+                  return DataTableWidget(
+                            data: state.filteredJobs.map((job) => job.toJson()).toList(),
+                            containerWidth: containerWidth,
+                            rowsPerPage: dataTableState.rowsPerPage,
+                            currentPage: dataTableState.currentPage,
+                            totalPages: totalPages,
+                            totalHits: state.totalHits,
+                            availableRowsPerPage: const [5, 10, 25, 50],
+                            onPageChanged: (newPage) => context.read<DataTableBloc>().add(ChangePageEvent(newPage)),
+                            onRowsPerPageChanged: (newRowsPerPage) =>
+                                context.read<DataTableBloc>().add(ChangeRowsPerPageEvent(newRowsPerPage!)),
+                            columns: getTableColumns(containerWidth)
+                          );
+                }
+              );
+            }
     );
   }
 
@@ -144,46 +146,4 @@ class JobListingsView extends StatelessWidget {
     ];
   }
 
-  Widget desktopView(
-    BuildContext context,
-    int totalPages,
-    double containerWidth,
-    double filterWidth,
-    JobListingsState state,
-    FilterState filterState,
-    DataTableState dataTableState,
-  ) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        buildDataTableWidget(state, containerWidth, dataTableState, totalPages, context),
-        const SizedBox(width: 50), // space between table and filter
-        buildFilterWidget(state, filterWidth, context),
-      ],
-    );
-  }
-
-  Widget phoneView(
-    BuildContext context,
-    int totalPages,
-    double mediaWidth,
-    JobListingsState state,
-    FilterState filterState,
-    DataTableState dataTableState,
-    {int filtersPerLine = 1}
-  ) {
-    return Container(
-      alignment: Alignment.center,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          buildFilterWidget(state, mediaWidth, context, filtersPerLine: filtersPerLine),
-          const SizedBox(height: 50), // space between table and filter
-          buildDataTableWidget(state, mediaWidth, dataTableState, totalPages, context),
-        ],
-      ),
-    );
-  }
 }
--- data_table_template/lib/main.dart
+++ data_table_template/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:job_listings/jobs_page.dart';
 import 'package:job_listings/theme_collection.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: CollectionTheme.getCollectionTheme(), // Apply the custom theme
-      home: JobListingsPage(),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: const JobListingsPage(),
+
+        breakpoints: [
+            const Breakpoint(start: 0, end: 360, name: 'SMALL_MOBILE'),
+            const Breakpoint(start: 361, end: 450, name: MOBILE),
+            const Breakpoint(start: 451, end: 800, name: TABLET),
+            const Breakpoint(start: 801, end: 1100, name: 'SMALL_DESKTOP'),
+            const Breakpoint(start: 1101, end: 1920, name: DESKTOP),
+            const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
     );
   }
 }
